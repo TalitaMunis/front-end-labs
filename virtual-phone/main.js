99544e6664e332a5bf1365b8f917e706854857d4
TalitaMunis <talitamunis72@gmail.com>
2025-05-27 21:38:02 -0300

    virtual phone parte final

--- virtual-phone/main.js
+++ virtual-phone/main.js
@@ -1,23 +1,28 @@
-const display = document.querySelector("input[type=tel]");
+const display = 
+    document.querySelector('input[type=tel]');
 
-const teclas = document.querySelectorAll("input[type=button]");
+const teclas = 
+    document.querySelectorAll('input[type=button]');
 
-const limpar = document.querySelector(".icon");
+const limpar = document.querySelector('.icon');
 
 limpar.onclick = () => {
-  const opcao = confirm("Deseja mesmo limpar?");
-  if (opcao) {
-    display.value = "";
+    const opcao = confirm('Deseja mesmo limpar?');
+    if (opcao) {
+        display.value = "";
+    }    
+}
+
+function valorTeclaToDisplay(i) {
+  if(display.value.length < 14){
+    display.value += teclas[i].value;
   }
-};
-
-//criar uma condição para travar mais de 14 digitos
-function valorTeclasToDisplay(i) {
-  display.value += teclas[i].value;
 }
 
-for (let i = 0; i < teclas.length; i++) {
-  teclas[i].onclick = () => {
-    valorTeclasToDisplay(i);
-  };
+for(let i = 0; i < teclas.length; i++){
+    teclas[i].onclick = () => {
+        valorTeclaToDisplay(i);
+    };
 }
+
+
